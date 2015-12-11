04ff111ed36d245130868b18a0ceb5b1b7bab40b
nandayou <dylan.nore@hotmail.com>
2015-12-11 14:44:34 +0100

    Revert "Alexander branch"

---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/WinScreen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class WinScreen : MonoBehaviour {
+
+	public Button MainMenu;
+	public Button QuitGame;
+	public Button NextLevel;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		MainMenu = MainMenu.GetComponent<Button> ();
+		QuitGame = QuitGame.GetComponent<Button> ();
+		NextLevel = NextLevel.GetComponent<Button> ();
+
+
+	}
+	
+	public void StartScreenReturn()
+
+	{
+
+		Application.LoadLevel (1);
+
+	}
+
+	public void GameTurnOff()
+
+	{
+
+		Application.Quit ();
+
+	}
+
+	public void NextGame()
+	{
+		Application.LoadLevel (2);
+	}
+
+}
