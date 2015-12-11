0d15456b6a5a211b90941a98a7e163a92c335f10
nandayou <dylan.nore@hotmail.com>
2015-12-11 14:54:35 +0100

    Testade merge, funkade ej.

---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameOver : MonoBehaviour {
+
+
+	public Button quitGame;
+
+	public Button mainMenu;
+
+
+	void Start () 
+	{
+
+		quitGame = quitGame.GetComponent<Button> ();
+		mainMenu = mainMenu.GetComponent<Button> ();
+	
+	}
+	
+	public void MainMenu()
+
+	{
+
+		Application.LoadLevel (1);
+
+	}
+
+	public void ExitGame()
+
+	{
+
+		Application.Quit ();
+
+	}
+
+
+}
---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/LifeCounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LifeCounter : MonoBehaviour {
+   
+    public Text textLives;
+   
+
+   	public void LifeChange(int lifenumber)
+    { 
+            textLives.text = "x " + lifenumber.ToString();
+        
+    }
+
+    
+}
