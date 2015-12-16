cc2bb26dd5b3e1f2749dcff6ed22a24d48a74d3c
Alexander Lövberg <leafmountainmail@gmail.com>
2015-12-16 13:01:52 +0100

    Ny version

--- Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/Movement.cs
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/Movement.cs
@@ -3,25 +3,17 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    bool move;  //Helt onödig!!
+    bool move = false;
 
     // Floats.orward-wall detection. I found .505f to be more precise than .5f.
     float rayLengthZ = 0.505f; // This ray is for f
 
-    float speed;
+    float speed = 1.5f;
     float rayLengthX = 0.6f; // The ray is cast from pacmans center, with 0.5f to closest SIDE-wall, so I wen
 
 
-    void OnEnable()
-    {
-        //transform.position = new Vector3(0.0f, 0.1f, -3.0f);
-        move = false; // To start out without movement.
-        speed = 1.5f; // Initial speed for pacman.
-    }
-
     void Update()
     {
-
 
         //First, I initialise three Rays going forward and sideways to check wall-collisions.
         Ray rayForward = new Ray(transform.position, transform.TransformDirection(0.0f, 0.5f, 0.5f));
@@ -30,27 +22,18 @@ public class Movement : MonoBehaviour {
         Ray rayLeft = new Ray(transform.position, transform.TransformDirection(-0.6f, 0.0f, -0.1f));
         RaycastHit hitinfo;
 
-        // Then, I move the player forward, unless the ray detects a collision near the front of pacman, and only if 'move' is set to 'true'.
+        Debug.DrawRay(transform.position, transform.TransformDirection(0.0f, 0.5f, 0.5f));
+
+
         if (!Physics.Raycast(rayForward, out hitinfo, rayLengthZ))
         {
-            if (move)
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || move)
             {
                 transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            }
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-            {
                 move = true;
             }
         }
-        
-            else if (hitinfo.collider.gameObject.tag == "navpoint")
-            {
-                if (move)
-                {
-                    transform.Translate(Vector3.forward * Time.deltaTime * speed);
-                }
-            }
-        
+
 
         // Pacman also needs a function to turn around.
         if (!Physics.Raycast(rayBackward, out hitinfo, rayLengthZ))
@@ -58,9 +41,6 @@ public class Movement : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             {
                 transform.Rotate(Vector3.down * 180.0f);
-                transform.position = new Vector3(Mathf.Round(transform.position.x),
-                                                 transform.position.y,
-                                                 Mathf.Round(transform.position.z));
             }
         }
 
@@ -70,9 +50,7 @@ public class Movement : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             {
                 transform.Rotate(Vector3.down * 90.0f);
-                transform.position = new Vector3(Mathf.Round(transform.position.x),
-                                                 transform.position.y,
-                                                 Mathf.Round(transform.position.z));
+
             }
         }
         // This checks for any walls to the right, which disallows a right-turn.
@@ -81,9 +59,6 @@ public class Movement : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             {
                 transform.Rotate(Vector3.up * 90.0f);
-                transform.position = new Vector3(Mathf.Round(transform.position.x),
-                                                 transform.position.y,
-                                                 Mathf.Round(transform.position.z));
             }
         }
     }
