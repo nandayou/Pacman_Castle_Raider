1459ae70f6f2c46b42fecd10676de9f7dce6058f
nandayou <dylan.nore@hotmail.com>
2015-12-20 21:21:48 +0100

    spikes/sword fix

---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/SwingThreeTimes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingThreeTimes : MonoBehaviour {
+	public int swingpower;
+	// Use this for initialization
+	void Start () {
+		ICanSwingThreeTimes ();
+	}
+
+	public void ICanSwingThreeTimes(){
+		swingpower = 3;
+		StartCoroutine (StartSwing ());
+	}
+
+	IEnumerator StartSwing()
+	{
+		while (swingpower > 0) {
+			if (Input.GetKeyDown (KeyCode.F)) {
+				GetComponent<Animation> ().Play ();
+				swingpower -= 1;
+			}
+		}
+		yield return new WaitForSeconds(1f);
+	}
+}
