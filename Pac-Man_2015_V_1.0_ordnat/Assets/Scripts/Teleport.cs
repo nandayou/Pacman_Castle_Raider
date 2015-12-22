de5a20b80f0342a35a0ad33fe74cde5ee697777c
nandayou <dylan.nore@hotmail.com>
2015-12-22 02:00:05 +0100

    golv overlappar inte, teleport skript

---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Scripts/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+
+	public float rotation;
+	public GameObject pacman;
+
+	public float x;
+	public float y;
+	public float z;
+
+	void Start () {
+
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject == pacman) {
+			pacman.transform.position = new Vector3(x,y,z);
+			pacman.transform.Rotate(0f, rotation, 0f);
+		}
+	}
+}
