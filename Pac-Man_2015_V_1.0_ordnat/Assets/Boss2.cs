947b11d365490b79ec51150e449168e275d73b3c
Alexander Lövberg <leafmountainmail@gmail.com>
2016-01-05 12:56:29 +0100

    Boss2-5

---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/Boss2.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss2 : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    GameObject hero;
+
+    float originalSpeed;
+    public float slowSpeedMulti = 0.7f;
+    public float slowTime = 5;
+    public float chargeSpeedMulti = 2f;
+
+    float timer;
+
+    float rayHeight;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        hero = GameObject.FindGameObjectWithTag("Player");
+
+        originalSpeed = agent.speed;
+        agent.destination = hero.transform.position;
+        rayHeight = hero.transform.position.y + hero.GetComponent<Renderer>().bounds.size.y;
+
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            agent.destination = hero.transform.position;
+        }
+
+        Timer();
+
+        if (LineOfSight() && timer <= 0)
+        {
+            agent.speed = originalSpeed * chargeSpeedMulti;
+        }
+        else if (agent.speed != originalSpeed && timer <= 0)
+        {
+            agent.speed = originalSpeed;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == hero)
+        {
+            agent.speed = originalSpeed * slowSpeedMulti;
+            timer = slowTime;
+        }
+    }
+
+    void Timer()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+
+    bool LineOfSight()
+    {
+        Ray eyesForward = new Ray(new Vector3(transform.position.x, rayHeight, transform.position.z), Vector3.forward);
+        Ray eyesRight = new Ray(new Vector3(transform.position.x, rayHeight, transform.position.z), Vector3.right);
+        Ray eyesBack = new Ray(new Vector3(transform.position.x, rayHeight, transform.position.z), Vector3.back);
+        Ray eyesLeft = new Ray(new Vector3(transform.position.x, rayHeight, transform.position.z), Vector3.left);
+        RaycastHit hit;
+
+        if (Physics.Raycast(eyesForward, out hit, Mathf.Infinity) && hit.transform.gameObject == hero || Physics.Raycast(eyesRight, out hit, Mathf.Infinity) && hit.transform.gameObject == hero || Physics.Raycast(eyesBack, out hit, Mathf.Infinity) && hit.transform.gameObject == hero || Physics.Raycast(eyesLeft, out hit, Mathf.Infinity) && hit.transform.gameObject == hero)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Pac-Man_2015_V_1.0_ordnat/Assets/LifeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeSpawner : MonoBehaviour {
+
+    GameObject[] navpoints;
+    float spawnTimer;
+
+    public GameObject visual;
+    public float spawnTime;
+
+    void Start()
+    {
+        navpoints = GameObject.FindGameObjectsWithTag("navpoint");
+    }
+
+    void PowerUpSpawn()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
+        {
+            Instantiate(visual, navpoints[Random.Range(0, navpoints.Length)].transform.position, Quaternion.identity);
+            spawnTimer = spawnTime;
+        }
+    }
+}
